efee82a8478f961eeea633eeac7c1cb24a59bb44
haroonarfan <haroonarfan109@gmail.com>
2023-01-12 15:07:47 +0000

    Haroon

---
+++ Meeting.py
@@ -0,0 +1,26 @@
+class Game():
+    def __int__(self):
+        self.board = [[]]  # 5 x 6 array
+
+    # Mridul
+    def check_victory(self, move):
+        pass
+
+    # Haroon
+    def make_move(self):
+        pass
+    # return valid move and board after making a move.
+    # contains a subroutine which returns all possible valid moves
+
+    # //
+    def start_game(self):
+        pass
+
+    # //
+    def players(self):
+        pass
+    # 2
+
+    # //
+    def robot(self):
+        pass
