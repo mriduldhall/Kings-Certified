91b1edb1201d2a701f82fe9f07895b7087e4a6b6
haroonarfan <haroonarfan109@gmail.com>
2023-01-14 19:29:44 +0000

    The class made in the meeting with methods discussed so far.

--- Meeting.py
+++ Meeting.py
@@ -6,7 +6,7 @@ class Game:
     def check_victory(self, move):
         pass
 
-    # Haroon
+    # Haroo
     def make_move(self):
         pass
     # return valid move and board after making a move.
