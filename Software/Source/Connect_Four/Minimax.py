91910adcdae386426c752d18c308cbbfb1840da3
mriduldhall <mriduldhall1@gmail.com>
2023-02-21 21:11:35 +0000

    Changed minimax to store series of moves in the tree instead of board state

--- Software/Source/Connect_Four/Minimax.py
+++ Software/Source/Connect_Four/Minimax.py
@@ -26,7 +26,7 @@ class Minimax:
             return score
         return (max if is_maximising else min)(
             self.minimax(possible_state, not is_maximising)
-            for possible_state in self.possible_new_states(deepcopy(state), is_maximising)
+            for possible_state, column in self.possible_new_states(deepcopy(state), is_maximising)
         )
 
     def best_move_tree(self, state):
@@ -35,40 +35,42 @@ class Minimax:
         nodes = []
         for column in current_board.get_valid_moves():
             current_board.make_move(column, self.maximising_marker)
-            score, child_node = self.minimax_tree(current_board.grid, False)
+            score, child_node = self.minimax_tree(current_board.grid, False, str(column))
             nodes.append(child_node)
             scores.append((score, column))
             current_board.grid = deepcopy(state)
-        root_node = Node(name=str(state), children=nodes, score=1)
+        root_node = Node(name=str(" "), children=nodes, score=1)
         self.tree = root_node
         return scores
 
-    def minimax_tree(self, state, is_maximising):
+    def minimax_tree(self, state, is_maximising, previous_moves):
         if (score := self.evaluate(state)) is not None:
-            node = Node(name=str(state), score=str(score))
+            node = Node(name=str(previous_moves), score=str(score))
             return score, node
         scores = []
         nodes = []
-        for possible_state in self.possible_new_states(deepcopy(state), is_maximising):
-            score, child_node = self.minimax_tree(possible_state, not is_maximising)
+        for possible_state, column in self.possible_new_states(deepcopy(state), is_maximising):
+            score, child_node = self.minimax_tree(possible_state, not is_maximising, previous_moves + str(column))
             nodes.append(child_node)
             scores.append(score)
         if is_maximising:
-            node = Node(name=str(state), score=str(max(scores)), children=nodes)
+            node = Node(name=str(previous_moves), score=str(max(scores)), children=nodes)
             return max(scores), node
         else:
-            node = Node(name=str(state), score=str(min(scores)), children=nodes)
+            node = Node(name=str(previous_moves), score=str(min(scores)), children=nodes)
             return min(scores), node
 
     def possible_new_states(self, state, is_maximising):
         current_board = Board(*self.game_setup_arguments, deepcopy(state))
         player_marker = self.maximising_marker if is_maximising else self.minimising_marker
         possible_states = []
+        columns = []
         for column in current_board.get_valid_moves():
             current_board.make_move(column, player_marker)
             possible_states.append(current_board.grid)
+            columns.append(column)
             current_board.grid = deepcopy(state)
-        return possible_states
+        return zip(possible_states, columns)
 
     def evaluate(self, state):
         current_board = Board(*self.game_setup_arguments, deepcopy(state))
@@ -93,24 +95,25 @@ class Minimax:
 
 if __name__ == '__main__':
     a = Board(rows=6, columns=7, empty=0, player_1=1, player_2=2, robot='R')
-    # a.grid = [
-    #     [0, 0, 0, 0, 0, 0, 0],
-    #     [2, 0, 0, 1, 2, 1, 1],
-    #     [1, 2, 1, 2, 1, 2, 2],
-    #     [2, 2, 1, 2, 1, 2, 1],
-    #     [1, 1, 2, 1, 1, 2, 2],
-    #     [1, 2, 1, 2, 2, 1, 1],
-    # ]
     a.grid = [
-        [2, 1, 2, 2, 0, 0, 0],
-        [2, 2, 1, 1, 2, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0],
+        [2, 0, 0, 1, 2, 1, 1],
         [1, 2, 1, 2, 1, 2, 2],
         [2, 2, 1, 2, 1, 2, 1],
         [1, 1, 2, 1, 1, 2, 2],
         [1, 2, 1, 2, 2, 1, 1],
     ]
+    # a.grid = [
+    #     [2, 1, 2, 2, 0, 0, 0],
+    #     [2, 2, 1, 1, 2, 1, 1],
+    #     [1, 2, 1, 2, 1, 2, 2],
+    #     [2, 2, 1, 2, 1, 2, 1],
+    #     [1, 1, 2, 1, 1, 2, 2],
+    #     [1, 2, 1, 2, 2, 1, 1],
+    # ]
     b = Minimax(1, 2, [6, 7, 0, 1, 2, "R"])
     moves = b.best_move_tree(a.grid)
     print(moves)
     print("Best move:", max(moves))
+    b.store_tree()
     print_tree(b.tree, attr_list=["score"])
