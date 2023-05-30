e04023279e5a5c01adfad98590bade6598070aff
Haroon Arfan <69432894+Haroon-Arfan@users.noreply.github.com>
2023-05-30 17:11:09 +0100

    Bug fixed: Depth changed from static [1] to dynamic.

--- Software/Source/Connect_Four/Minimax_heuristic.py
+++ Software/Source/Connect_Four/Minimax_heuristic.py
@@ -11,45 +11,39 @@ class Minimax:
         self.storage_function = TreeStorageFunction()
         self.current_line = 1
         self.current_depth = 1
+        self.max_depth = 7
 
     def best_move(self, state):
-        self.storage_function.max_depth = sum([row.count(self.game_setup_arguments[2]) for row in state])
-        self.storage_function.initialise_files()
         current_board = Board(*self.game_setup_arguments, self.deepcopy(state))
         scores = []
         for column in current_board.get_valid_moves():
             current_board.make_move(column, self.maximising_marker)
-            score, line_number = self.minimax(current_board.grid, False, str(column), str(column), 1)
+            score = self.minimax(current_board.grid, False, str(column), str(column), 1)
             scores.append((column, score))
             current_board.grid = self.deepcopy(state)
-        self.storage_function.close_files()
         return scores
 
     def minimax(self, state, is_maximising, previous_move, previous_moves, depth):
-        if (score := self.evaluate(state)) is not None:
-            line_number = self.storage_function.write_node(str(previous_move), str(score), depth)
-            return score, line_number
+        if depth >= self.max_depth:
+            score = self.evaluate(state)
+            return score
         scores = []
         lines_used = 0
-        line_number = 1
         for possible_state, column in self.possible_new_states(state, is_maximising):
-            score, line_number = self.minimax(possible_state, not is_maximising, column, previous_moves + str(column), depth + 1)
+            score = self.minimax(possible_state, not is_maximising, column, previous_moves + str(column), depth + 1)
             if score is not None:
                 scores.append(score)
                 lines_used += 1
                 if (is_maximising and score == float('inf')) or (not is_maximising and score == float('-inf')):
                     break
-        line_number = str(int(line_number) - lines_used + 1)
         if is_maximising:
             if scores:
-                line_number = self.storage_function.write_node(str(previous_move), str(max(scores)), depth, line_number)
-                return max(scores), line_number
-            return None, None
+                return max(scores)
+            return None
         else:
             if scores:
-                line_number = self.storage_function.write_node(str(previous_move), str(min(scores)), depth, line_number)
-                return min(scores), line_number
-            return None, None
+                return min(scores)
+            return None
 
     def possible_new_states(self, state, is_maximising):
         current_board = Board(*self.game_setup_arguments, self.deepcopy(state))
@@ -63,16 +57,6 @@ class Minimax:
             current_board.grid = self.deepcopy(state)
         return zip(possible_states, columns)
 
-    # def evaluate(self, state):
-    #     current_board = Board(*self.game_setup_arguments, state)
-    #     victory_status = current_board.check_victory()
-    #     if victory_status[0] is True or len(current_board.get_valid_moves()) == 0:
-    #         if victory_status[1] == self.maximising_marker:
-    #             return 1
-    #         elif victory_status[1] == self.minimising_marker:
-    #             return -1
-    #         return 0
-    #     return None
 
     @staticmethod
     def deepcopy(state):
@@ -252,7 +236,7 @@ if __name__ == '__main__':
     # a.grid = [
     #     [2, 1, 2, 2, 0, 0, 0],
     #     [2, 2, 1, 1, 2, 1, 1],
-    #     [1, 2, 1, 2, 1, 2, 2],
+    #     [1, 2, 1, 2, 1, 2, 1],
     #     [2, 2, 1, 2, 1, 2, 1],
     #     [1, 1, 2, 1, 1, 2, 2],
     #     [1, 2, 1, 2, 2, 1, 1],
@@ -264,6 +248,14 @@ if __name__ == '__main__':
     #     [2, 2, 1, 2, 1, 2, 1],
     #     [1, 1, 2, 1, 1, 2, 2],
     #     [1, 2, 1, 2, 2, 1, 1],
+    # ]
+    # a.grid = [
+    # [0, 0, 0, 0, 0, 0, 0],
+    # [2, 0, 0, 1, 2, 1, 1],
+    # [1, 2, 1, 2, 1, 2, 2],
+    # [2, 2, 1, 2, 1, 2, 1],
+    # [1, 1, 2, 1, 1, 2, 2],
+    # [1, 2, 1, 2, 2, 1, 1],
     # ]
 
     b = Minimax(1, 2, [6, 7, 0, 1, 2, "R"])
